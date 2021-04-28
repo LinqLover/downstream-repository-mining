51490ccbc6b8404b1c7ce1e8aaa9759a432e7b78
Christoph Thiede <christoph.thiede@student.hpi.de>
2021-04-28 23:09:24 +0200

    Fix indentations

--- src/npm-deps.ts
+++ src/npm-deps.ts
@@ -28,68 +28,68 @@ class GitHubRepository {
 
 
 export async function getNpmDeps(packageName: string, limit: number, countNestedDepedents = false) {
-	dotenv.config()
+    dotenv.config()
 
-	const endpoint = 'https://api.github.com/graphql'
+    const endpoint = 'https://api.github.com/graphql'
 
-	const graphQLClient = new GraphQLClient(endpoint, {
-		headers: {
-			authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
-			accept: 'application/vnd.github.hawkgirl-preview+json'
-		},
-	})
+    const graphQLClient = new GraphQLClient(endpoint, {
+        headers: {
+            authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
+            accept: 'application/vnd.github.hawkgirl-preview+json'
+        },
+    })
 
-	let dependents = await asyncIteratorToArray(getNpmDependents(packageName, limit))
+    let dependents = await asyncIteratorToArray(getNpmDependents(packageName, limit))
 
-	for (const dependent of tqdm(dependents, {desc: "Gathering metadata"})) {
-		const metadata = await RegistryClient.getMetadata(dependent.name, {fullMetadata: true})
-		const url = metadata?.versions?.latest?.repository?.url
-		const match = url?.match(/github\.com[\/:](?<owner>[^/]+)\/(?<name>[\w-_\.]+?)(?:.git)?$/)
-		if (!match) {
+    for (const dependent of tqdm(dependents, {desc: "Gathering metadata"})) {
+        const metadata = await RegistryClient.getMetadata(dependent.name, {fullMetadata: true})
+        const url = metadata?.versions?.latest?.repository?.url
+        const match = url?.match(/github\.com[\/:](?<owner>[^/]+)\/(?<name>[\w-_\.]+?)(?:.git)?$/)
+        if (!match) {
             console.warn("Package has no GitHub link", {metadata, url})
-			continue
-		}
-		dependent.github = new GitHubRepository(
-			match.groups.owner,
-			match.groups.name
-		)
-	}
+            continue
+        }
+        dependent.github = new GitHubRepository(
+            match.groups.owner,
+            match.groups.name
+        )
+    }
 
-	for (const repo of tqdm(dependents, {desc: "Gathering GitHub data"})) {
-		if (!repo.github) continue
-		const repoData = (await getRepoData(graphQLClient, repo.github.owner, repo.github.name)).repository
-		Object.assign(repo.github, repoData);
-	}
+    for (const repo of tqdm(dependents, {desc: "Gathering GitHub data"})) {
+        if (!repo.github) continue
+        const repoData = (await getRepoData(graphQLClient, repo.github.owner, repo.github.name)).repository
+        Object.assign(repo.github, repoData);
+    }
 
-	if (countNestedDepedents) {
-		for (const dependent of tqdm(dependents, {desc: "Gathering nested dependents data"})) {
-			dependent.dependentCount = (await asyncIteratorToArray(getNpmDependents(dependent.name, limit))).length;
-		}
-	}
+    if (countNestedDepedents) {
+        for (const dependent of tqdm(dependents, {desc: "Gathering nested dependents data"})) {
+            dependent.dependentCount = (await asyncIteratorToArray(getNpmDependents(dependent.name, limit))).length;
+        }
+    }
 
-	dependents = dependents.sort((a, b) => (a.github?.stargazerCount ?? 0) - (b.github?.stargazerCount ?? 0))
+    dependents = dependents.sort((a, b) => (a.github?.stargazerCount ?? 0) - (b.github?.stargazerCount ?? 0))
 
-	return dependents
+    return dependents
 }
 
 async function* getNpmDependents(packageName: string, limit: number | null) {
     let count = 0
     for await (const dependent of npmDependants(packageName)) {
-		yield <Dependent>{name: <string>dependent, github: undefined, dependentCount: undefined}
-		if (limit && ++count >= limit) break;
-	}
+        yield <Dependent>{name: <string>dependent, github: undefined, dependentCount: undefined}
+        if (limit && ++count >= limit) break;
+    }
 }
 
 async function getRepoData(graphQLClient: GraphQLClient, owner: string, name: string) {
-	const query = gql`
-		query githubDeps($owner: String!, $name: String!) {
-			repository(owner: $owner, name: $name) {
-				url, stargazerCount, forkCount
-			}
-		}
-	`
+    const query = gql`
+        query githubDeps($owner: String!, $name: String!) {
+            repository(owner: $owner, name: $name) {
+                url, stargazerCount, forkCount
+            }
+        }
+    `
 
-	const variables = {owner, name}
+    const variables = {owner, name}
 
-	return await graphQLClient.request(query, variables)
+    return await graphQLClient.request(query, variables)
 }
