5b501af5798a85e1603fe7cf6c630ec3dfca444f
Christoph Thiede <christoph.thiede@outlook.de>
2021-05-12 11:45:29 +0000

    Add smoke test for CLI
    Due to repeated breakdowns of the oclif functionality in the past,
    presumably caused by faulty module import configurations, we introduce a
    simple smoke test that will make the CI fail if the CLI does not provide
    at least 2 commands. See also 74fe4be.

---
+++ scripts/test-cli.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+set -e
+
+fail() {
+    >&2 echo "$1"
+    exit 1
+}
+
+output="$(./bin/run.js help | tee /dev/tty)"
+
+commands="$(echo "$output" | sed -n '/^COMMANDS$/,$p' | sed -n '/^  /p')"
+number_of_commands="$(echo "$commands" | wc -l)"
+
+(( $number_of_commands > 10 )) || fail "Not enough commands provided"
