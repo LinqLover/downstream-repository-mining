bcbfbdd98b865f38e369b94e60ca4a728a3c919e
Christoph Thiede <christoph.thiede@student.hpi.de>
2021-06-22 15:40:37 +0200

    Try to fix tput for CI (2)

--- test/references.test/self-test.sh
+++ test/references.test/self-test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 export TERM="${TERM:-xterm-color}"  # for tput
+echo "term is $TERM"  # DEBUG
 
 op() {
-    tput bold && echo "\$ $*" && tput sgr0
+    tput bold ; echo "\$ $*" ; tput sgr0
     "$@"
 }
 
