d42f38304ee0c180fbdd10f506b1b49f37d2295c
Christoph Thiede <christoph.thiede@outlook.de>
2021-07-06 20:37:16 +0000

    example: Add import with nested imports

--- packages/example/index.ts
+++ packages/example/index.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env -S npx ts-node
 
 import Package from "core/src/core/package"
+import { FilePosition } from "core/src/core/references"
 
 const $package = new Package({
     name: 'name',
     directory: 'directory'
 })
+const position = new FilePosition({
+    row: 42
+})
 console.log($package)
+console.log(position)
