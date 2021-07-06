b31ca68d1ad9f18b3541b64fba74b0274f5101a4
Christoph Thiede <christoph.thiede@outlook.de>
2021-07-06 20:46:39 +0000

    Complete coverage of core package in example package

--- packages/example/index.ts
+++ packages/example/index.ts
@@ -1,8 +1,13 @@
 #!/usr/bin/env -S npx ts-node
 
+import { Dependent } from "core/src/core/npm-deps"
 import Package from "core/src/core/package"
 import { FilePosition } from "core/src/core/references"
 
+const dependent = new Dependent({
+    name: 'foo',
+    tarballUrl: 'https://example.com'
+})
 const $package = new Package({
     name: 'name',
     directory: 'directory'
@@ -10,5 +15,6 @@ const $package = new Package({
 const position = new FilePosition({
     row: 42
 })
+console.log(dependent)
 console.log($package)
 console.log(position)
