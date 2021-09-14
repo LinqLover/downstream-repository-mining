49e76068c4622e968395d6375a8f8a6ffb96c557
Christoph Thiede <christoph.thiede@outlook.de>
2021-09-14 09:50:25 +0000

    Extend acceptance tests for CLI

--- packages/cli/scripts/test-cli.sh
+++ packages/cli/scripts/test-cli.sh
@@ -6,10 +6,38 @@ fail() {
     exit 1
 }
 
-output="$(./bin/run.js help)"
+bin="./bin/run.js"
+
+
+# First round: Does the help output look plausible?
+output="$($bin help)"
 echo "$output"
 
 commands="$(echo "$output" | sed -n '/^COMMANDS$/,$p' | sed -n '/^  /p')"
 number_of_commands="$(echo "$commands" | wc -l)"
 
-(( number_of_commands > 4 )) || fail "Not enough commands provided"
+(( number_of_commands > 4 )) || fail "not enough commands provided"
+
+
+# Second round: Let's do a real world acceptance test
+packageName="cheerio"
+export NPM_CACHE="cache-test"
+
+output=$($bin list --limit 10 --no-downloadGitHubData $packageName)
+echo "$output"
+[[ $output == *'tarballUrl:'* ]] || fail "list does not output valid hits"
+
+output=$($bin download --limit 10 $packageName)
+echo "$output"
+[[ $output == *'Download completed, 10 successful'* ]] ||fail "download was not successful"
+
+output=$($bin search --limit 10 --strategy heuristic $packageName)
+echo "$output"
+[[ $output == *'matchString:'* ]] || fail "search heuristic does not output valid hits"
+
+export NPM_CACHE_SRC="$NPM_CACHE-src"
+mkdir $NPM_CACHE_SRC
+NPM_CACHE="$NPM_CACHE-src" "$(dirname "$0")/download-package.sh" $packageName
+output=$($bin search --limit 10 --strategy types --source "$NPM_CACHE_SRC/$packageName" $packageName)
+echo "$output"
+[[ $output == *'matchString:'* ]] || fail "search types does not output valid hits"
