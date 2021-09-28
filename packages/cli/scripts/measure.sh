b7898fc6cf8a22bff5dc3d324a4424e7c3d0309d
Christoph Thiede <christoph.thiede@outlook.de>
2021-09-28 00:14:54 +0000

    Adjust paths for measure.sh

--- packages/cli/scripts/measure.sh
+++ packages/cli/scripts/measure.sh
@@ -6,10 +6,10 @@
 #   SOURCEGRAPH_TOKEN
 
 # measure time
-time sh -c "./bin/run.js list $1 --strategies=npm --no-downloadGitHubData --limit -1 > output-$1-npm.txt 2> error-$1-npm.txt"
-time sh -c "./bin/run.js list $1 --strategies=sourcegraph --no-downloadGitHubData --limit -1 > output-$1-sourcegraph.txt 2> error-$1-sourcegraph.txt"
+time sh -c "./bin/run.js list $1 --strategies=npm --no-downloadGitHubData --limit -1 > output/output-$1-npm.txt 2> output/error-$1-npm.txt"
+time sh -c "./bin/run.js list $1 --strategies=sourcegraph --no-downloadGitHubData --limit -1 > output/output-$1-sourcegraph.txt 2> output/error-$1-sourcegraph.txt"
 
 # compute intersection
-grep '^  name:' "output-$1-npm.txt" > "pkgs-$1-npm.txt"
-grep '^  name:' "output-$1-sourcegraph.txt" > "pkgs-$1-sourcegraph.txt"
-awk 'a[$0]++' "pkgs-$1-npm.txt" "pkgs-$1-sourcegraph.txt" | wc -l
+grep '^  name:' "output/output-$1-npm.txt" > "output/pkgs-$1-npm.txt"
+grep '^  name:' "output/output-$1-sourcegraph.txt" > "output/pkgs-$1-sourcegraph.txt"
+awk 'a[$0]++' "output/pkgs-$1-{npm,sourcegraph}.txt" | wc -l
